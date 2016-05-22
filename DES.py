b499bf8f407a3ea703a6e8ea1f26e1b0613596cf
Jarrod <jarrob@udel.edu>
2016-05-22 14:52:45 -0400

    Final Code

--- DES.py
+++ DES.py
@@ -2,15 +2,15 @@
 # CAPSTONE Team Auto
 # Driver Evaluation System
 
-import testprog
+import evalScript
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
 
-# As the Raspberry Pi is powered on (vehicle ignition), first evaluate 
-# the OBD2 data from the previous driving session
-testprog.dataEval()
+	# As the Raspberry Pi is powered on (vehicle ignition), first evaluate 
+	# the OBD2 data from the previous driving session
+	evalScript.dataEval()
 
-# Then, in a continuous loop, start collecting data from the OBD2 reader
-# and the GPS adapter. Convert the data into a CSV file that gets placed in
-# the OBD_data folder. Do this until the vehicle is turned off.
-# (call Charlie's script here)
+	# Then, in a continuous loop, start collecting data from the OBD2 reader
+	# and the GPS adapter. Convert the data into a CSV file that gets placed in
+	# the OBD_data folder. Do this until the vehicle is turned off.
+	# (call Charlie's script here)
